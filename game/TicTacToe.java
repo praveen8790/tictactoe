417ea068f849aed01a8333c4ab03d27825a040cd
praveen8790 <praveen8978470998rock@gmail.com>
2021-05-25 12:22:59 +0530

    print board

--- game/TicTacToe.java
+++ game/TicTacToe.java
@@ -5,14 +5,13 @@ import java.util.Scanner;
 public class TicTacToe {
     public static Scanner scanner = new Scanner(System.in);
     public void board(char[] board){
-
         System.out.println(  board[1] + " | " + board[2] + " | " + board[3]);
         System.out.println("---------");
         System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
         System.out.println("---------");
         System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
     }
-   
+
 
     public static void main(String[] args) {
         char[] board = new char[10];
